e93ed3b1120e5929889d4717e266f0a6b30ed3be
Smarty Pants <amcurrent@hotmail.com>
2019-06-08 19:14:25 -0500

    worked on some logic

---
+++ memory/src/assets/js/logic.js
@@ -0,0 +1,38 @@
+import Main from "../../components/Main"
+const click = {
+    array: [],
+    score: 0,
+    check: (num) => {
+//ONLY FIRES ON CLICK!
+        if (!click.array[0]){
+            click.array.push(parseInt(num))
+            click.score += 1
+        } else {
+            for( var i = 0; i < click.array.length; i++){
+                
+                if (click.array[i] === num && click.score !== 12){
+                    alert("Oops! \nYou've already clicked that one! :'( \nTry again!");
+                    click.end()
+                } else if ( click.array[i] !== num && i === ( click.array.length - 1 )){
+                    click.array.push(parseInt(num));
+                    click.score += 1;
+                    if (click.score === 12){
+                        alert("Congratulations! \n      You managed to click on all of them! \n             Well done!")
+                        click.end();
+                        break
+                    }
+                    break
+                }
+                
+            }
+        }
+
+        console.log(click.array);
+    },
+    end: () => {
+        click.score = 0;
+        click.array = [];
+    }
+}
+
+export default click;
