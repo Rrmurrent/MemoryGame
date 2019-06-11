e0fe44f48bd3dc08ae22ec01406f3dbe46b8157b
Smarty Pants <amcurrent@hotmail.com>
2019-06-10 19:05:50 -0500

    moved stuff

---
+++ src/App.js
@@ -0,0 +1,28 @@
+    
+import React from "react"
+import NavBar from "./components/navBar"
+import Main from "./components/main"
+import Background from "./assets/images/trianglify.png"
+
+const background = {
+    width: "100%",
+    height: "100%",
+    backgroundImage: "url(" + Background + ")",
+    backgroundAttachment : "fixed"
+
+
+}
+
+const App = () => {
+  
+    return(
+        <div style = {background}>
+
+            <NavBar />
+            <Main />
+
+        </div>
+    )
+};
+
+export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () =>{
+    return (
+        <nav className = "jumbotron bg-dark text-white text-center navigate">
+            <div>
+                <h1>The Clicky Game!</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
